b0ff82ee27c527896dfb060dd2c49ce468e5333e
Jonathan Perry <jpper@umich.edu>
2019-11-09 17:35:08 +0900

    Removed old chart upon creation of new chart for implementing refresh functionality

--- web-ui/public/js/script.js
+++ web-ui/public/js/script.js
@@ -13,6 +13,7 @@ var humidityData = [];
 var pressureData = [];
 
 function createChart(sensor) {
+  d3.selectAll("svg > *").remove();
   nv.addGraph(function() {
     var chart = nv.models.lineChart();
 
@@ -78,29 +79,34 @@ function readData(sensor) {
   var db = firebase.firestore();
   var collectionRef = db.collection(sensor);
   collectionRef.orderBy("time").limit(50);
-  readSensorData(sensor).then(() => {
-    createChart("temperature");
-    createChart("humidity");
-    createChart("pressure");
-  });
+  readSensorData(sensor);
 
   // It doesn't matter whether this information is rendered after the chart data is created
-  collectionRef.get().then(querySnapshot => {
-    querySnapshot.forEach(doc => {
-      document.getElementById(sensor).innerText = doc.data().value;
-      var today = new Date();
-      var date =
-        today.getFullYear() +
-        "-" +
-        (today.getMonth() + 1) +
-        "-" +
-        today.getDate();
-      var time =
-        today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-      var dateTime = date + " " + time;
-      document.getElementById("last-update").innerText = dateTime;
+  collectionRef
+    .get()
+    .then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        document.getElementById(sensor).innerText = doc.data().value;
+        var today = new Date();
+        var date =
+          today.getFullYear() +
+          "-" +
+          (today.getMonth() + 1) +
+          "-" +
+          today.getDate();
+        var time =
+          today.getHours() +
+          ":" +
+          today.getMinutes() +
+          ":" +
+          today.getSeconds();
+        var dateTime = date + " " + time;
+        document.getElementById("last-update").innerText = dateTime;
+      });
+    })
+    .catch(err => {
+      console.error(err);
     });
-  });
 }
 
 function refreshData() {
@@ -117,17 +123,24 @@ function readSensorData(sensor) {
   var db = firebase.firestore();
   var collectionRef = db.collection(sensor);
   collectionRef.orderBy("time").limit(50);
-  return collectionRef.get().then(querySnapshot => {
-    querySnapshot.forEach(doc => {
-      if (sensor === "temperature") {
-        temperatureData.push(doc.data());
-      } else if (sensor === "humidity") {
-        humidityData.push(doc.data());
-      } else if (sensor === "pressure") {
-        pressureData.push(doc.data());
-      }
+  return collectionRef
+    .get()
+    .then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        if (sensor === "temperature") {
+          temperatureData.push(doc.data());
+        } else if (sensor === "humidity") {
+          humidityData.push(doc.data());
+        } else if (sensor === "pressure") {
+          pressureData.push(doc.data());
+        }
+      });
+    })
+    .then(() => {
+      createChart("temperature");
+      createChart("humidity");
+      createChart("pressure");
     });
-  });
 }
 
 /**
